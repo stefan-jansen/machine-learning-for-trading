cd351230dbeac13e8fe937e540e3dae74ac8e741
Stefan Jansen <stefan@applied-ai.solutions>
2019-04-20 15:20:24 -0400

    chapter 11 review

---
+++ 11_gradient_boosting_machines/gbm_utils.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = 'Stefan Jansen'
+
+from pathlib import Path
+import numpy as np
+import pandas as pd
+
+pd.set_option('display.expand_frame_repr', False)
+np.random.seed(42)
+
+"""
+This file contains several helper functions 
+used to prepare the data and run cross-validation
+"""
+
+DATA_STORE = Path('../data/assets.h5')
+
+def format_time(t):
+    """Return a formatted time string 'HH:MM:SS
+    based on a numeric time() value"""
+    m, s = divmod(t, 60)
+    h, m = divmod(m, 60)
+    return f'{h:0>2.0f}:{m:0>2.0f}:{s:0>2.0f}'
+
+
+def get_data(start='2000', end='2018', holding_period=1, dropna=False):
+    """Load dataset created in Chapter 4"""
+    idx = pd.IndexSlice
+    target = f'target_{holding_period}m'
+    with pd.HDFStore(DATA_STORE) as store:
+        df = store['engineered_features']
+
+    if start is not None and end is not None:
+        df = df.loc[idx[:, start: end], :]
+    if dropna:
+        df = df.dropna()
+
+    y = (df[target] > 0).astype(int)
+    X = df.drop([c for c in df.columns if c.startswith('target')], axis=1)
+    return y, X
+
+
+def get_one_hot_data(df, cols=('year', 'month', 'age', 'msize')):
+    cols = list(cols)
+    df = pd.get_dummies(df,
+                        columns=cols + ['sector'],
+                        prefix=cols + [''],
+                        prefix_sep=['_'] * len(cols) + [''])
+    return df.rename(columns={c: c.replace('.0', '').replace(' ', '_').lower() for c in df.columns})
+
+
+def factorize_cats(df, cats=['sector']):
+    cat_cols = ['year', 'month', 'age', 'msize'] + cats
+    for cat in cats:
+        df[cat] = pd.factorize(df[cat])[0]
+    df.loc[:, cat_cols] = df.loc[:, cat_cols].fillna(-1).astype('category')
+    return df
+
+
+def get_holdout_set(target, features, period=6):
+    idx = pd.IndexSlice
+    label = target.name
+    dates = np.sort(target.index.get_level_values('date').unique())
+    cv_start, cv_end = dates[0], dates[-period - 2]
+    holdout_start, holdout_end = dates[-period - 1], dates[-1]
+
+    df = features.join(target.to_frame())
+    train = df.loc[idx[:, cv_start: cv_end], :]
+    y_train, X_train = train[label], train.drop(label, axis=1)
+
+    test = df.loc[idx[:, holdout_start: holdout_end], :]
+    y_test, X_test = test[label], test.drop(label, axis=1)
+    return y_train, X_train, y_test, X_test
+
+
+class OneStepTimeSeriesSplit:
+    """Generates tuples of train_idx, test_idx pairs
+    Assumes the index contains a level labeled 'date'"""
+
+    def __init__(self, n_splits=3, test_period_length=1, shuffle=False):
+        self.n_splits = n_splits
+        self.test_period_length = test_period_length
+        self.shuffle = shuffle
+        self.test_end = n_splits * test_period_length
+
+    @staticmethod
+    def chunks(l, chunk_size):
+        for i in range(0, len(l), chunk_size):
+            yield l[i:i + chunk_size]
+
+    def split(self, X, y=None, groups=None):
+        unique_dates = (X.index
+                            .get_level_values('date')
+                            .unique()
+                            .sort_values(ascending=False)[:self.test_end])
+
+        dates = X.reset_index()[['date']]
+        for test_date in self.chunks(unique_dates, self.test_period_length):
+            train_idx = dates[dates.date < min(test_date)].index
+            test_idx = dates[dates.date.isin(test_date)].index
+            if self.shuffle:
+                np.random.shuffle(list(train_idx))
+            yield train_idx, test_idx
+
+    def get_n_splits(self, X, y, groups=None):
+        return self.n_splits
